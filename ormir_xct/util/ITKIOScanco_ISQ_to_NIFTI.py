2b11daae48b5f6ed1f549f817b18ccb1551dde8d
Sarah10109 <hilybob10109@gmail.com>
2025-05-16 15:46:32 -0600

    itkioscanco file converters

--- ormir_xct/util/ITKIOScanco_ISQ_to_NIFTI.py
+++ ormir_xct/util/ITKIOScanco_ISQ_to_NIFTI.py
@@ -3,22 +3,20 @@ import SimpleITK as sitk
 from ormir_xct.util.sitk_itk import itk_sitk
 
 #---------------#
-# AIM Reading
+# ISQ Reading
 #---------------#
-aim_read_path = "example/DYNACT2_214_TMC_MID.AIM"
-nifti_write_path = "test/test2.nii"
+isq_read_path = "example/test.isq"
+nifti_write_path = "tests/test2.nii"
 
 image_type = itk.Image[itk.ctype("signed short"), 3]
 reader = itk.ImageFileReader[image_type].New()
 image_io = itk.ScancoImageIO.New()
 reader.SetImageIO(image_io)
-reader.SetFileName(aim_read_path)
+reader.SetFileName(isq_read_path)
 reader.Update()
-
 
 #---------------#
 # NIFTI Writing
 #---------------#
 output_image = itk_sitk(reader.GetOutput())
 sitk.WriteImage(output_image, str(nifti_write_path))
-
